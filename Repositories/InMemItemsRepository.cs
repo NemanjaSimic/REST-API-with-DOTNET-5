5dfdaa5c894a2961558c83f8f02bfbc29613fb32
Nemanja Simic <snemanja.simic@gmail.com>
2021-07-01 22:40:11 +0200

    InMemory repo, item controller and get requests

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository : IItemsRepository
+    {
+        private readonly List<Item> items = new(3)
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Brozne Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+
+    }
+}
